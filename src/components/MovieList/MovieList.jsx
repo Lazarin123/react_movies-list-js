25da0b8ccca86592cf270cd2076c50b0217143c0
Samuel Lazarin <samuel.lazarin12@gmail.com>
2026-01-29 19:30:24 -0300

    add task solution test one

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,17 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard/MovieCard';
+import moviesFromServer from '../../api/movies.json';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = () => (
+  <div className="movies">
+    {moviesFromServer.map(({ imgUrl, imdbUrl, title, description, imdbId }) => (
+      <MovieCard
+        key={imdbId}
+        imgUrl={imgUrl}
+        imdbUrl={imdbUrl}
+        title={title}
+        description={description}
+      />
+    ))}
+  </div>
+);
